cd576001bdcb38e2934b463ff7bbd90848111ac6
Blaine Perston <96595637+aSouthGoingZax@users.noreply.github.com>
2022-02-14 20:05:04 +0000

    clean up

--- config.def.h
+++ config.def.h
@@ -11,28 +11,16 @@ static const char *colors[SchemeLast][2] = {
 	/* night owl */
 	[SchemeNorm]          = { "#d6deeb", "#011627" },
 	[SchemeSel]           = { "#011627", "#82aaff" },
-	[SchemeOut]           = { "#22da6e", "#011627" },    // the prompt uses this scheme
+	[SchemeOut]           = { "#22da6e", "#011627" },     // the prompt uses this scheme
 	[SchemeNormHighlight] = { "#c5e478", "#011627" },
 	[SchemeSelHighlight]  = { "#434242", "#82aaff" },
-	[SchemeOutHighlight]  = { "#00ff00", "#00ff00" },    // used only for muliple selections (ctrl+return), which I never do
-	/* tokyo night */
-	// [SchemeNorm]          = { "#a9b1d6", "#1a1b26" },
-	// [SchemeSel]           = { "#1a1b26", "#7dcfff" },
-	// [SchemeOut]           = { "#5feeb6", "#1a1b26" },    // the prompt uses this scheme
-	// [SchemeNormHighlight] = { "#d4deff", "#1a1b26" },
-	// [SchemeSelHighlight]  = { "#373e5e", "#7dcfff" },
-	// [SchemeOutHighlight]  = { "#00ff00", "#00ff00" },    // used only for muliple selections (ctrl+return), which I never do
+	[SchemeOutHighlight]  = { "#00ff00", "#00ff00" },     // used only for muliple selections (ctrl+return), which I never do
 };
-static const unsigned int baralpha = 0xe6;                // 230 in hexadecimal, 230/255=0.9
-static const unsigned int borderalpha = OPAQUE;           // this seemingly does nothing, all transparency is controlled by  baralpha
-static const unsigned int alphas[][3] = {
-	/*                        fg      bg        border     */
-	[SchemeNorm]          = { OPAQUE, baralpha, borderalpha },
-	// [SchemeSel]           = { OPAQUE, baralpha, borderalpha },
-	// [SchemeOut]           = { OPAQUE, baralpha, borderalpha },
-	// [SchemeNormHighlight] = { OPAQUE, baralpha, borderalpha },
-	// [SchemeSelHighlight]  = { OPAQUE, baralpha, borderalpha },
-	[SchemeOpaque]        = { OPAQUE, OPAQUE,   OPAQUE      },
+static const unsigned int alpha = 0xe6;                   // 230 in hexadecimal, 230/255=0.9
+static const unsigned int alphas[][2] = {
+	/*                 fg      bg    */
+	[SchemeNorm]   = { OPAQUE, alpha  },
+	[SchemeOpaque] = { OPAQUE, OPAQUE },
 };
 static unsigned int lines = 15;                           // -l option; dmenu uses vertical list with given number of lines if non-zero
 static const char worddelimiters[] = " ";                 // Characters not considered part of a word while deleting words, e.g. " /?\"&[]"
